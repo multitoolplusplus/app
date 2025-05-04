8496a9e41be53a0b80a315e74f7407130649b046
benja2998 <benja2998@users.noreply.github.com>
2025-05-04 16:11:50 -0400

    Update README.md

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
-using namespace std; // this will make the syntax better
+using namespace std; // this will make the syntax more readable
 
 int main()
 {
     cout << "Hello from Multitool++!\n";
+    cout << "C++ is a great language!\n";
+
+    return 0; // exit code success
 }
