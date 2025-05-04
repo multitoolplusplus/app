f587289aeb274a96f34a9d46df4b6bcea50714bb
benja2998 <benja2998@users.noreply.github.com>
2025-05-04 16:09:22 -0400

    Add main.cpp, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# multitoolplusplus
-multitool++
+# multitool++
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std; // this will make the syntax better
+
+int main()
+{
+    cout << "Hello from Multitool++!\n";
+}
