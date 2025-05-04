2186e38266cbd271585d7802468bb05fb950e108
benja2998 <benja2998@users.noreply.github.com>
2025-05-04 16:31:57 -0400

    Massive update to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+#include <iostream>                                                 // iostream contains std
 
-using namespace std;                                // using the std namespace for more readable syntax
+static void banner()
+{
+    const char* banner = R"(
+                  .__   __  .__  __                .__                           
+  _____  __ __|  |_/  |_|__|/  |_  ____   ____ |  |      .__         .__     
+ /     \|  |  \  |\   __\  \   __\/  _ \ /  _ \|  |    __|  |___   __|  |___ 
+|  Y Y  \  |  /  |_|  | |  ||  | (  <_> |  <_> )  |__ /__    __/  /__    __/ 
+|__|_|  /____/|____/__| |__||__|  \____/ \____/|____/    |__|        |__|    
+      \/                                                                     
+
+)";                                                                 // multiline raw string
+    std::cout << banner << "\n";                                    // cout - character output
+}
 
 int main()
 {
-    cout << "Hello from Multitool++!\n";            // cout - character output
-    cout << "C++ is a great language!\n";           // cout - character output
+    std::cout << "Hello from Multitool++!\n";                       // cout - character output
+    std::cout << "C++ is a great language!\n";                      // cout - character output
 
-    return 0;                                       // exit code success
+    banner();                                                       // calling banner function
+
+    return 0;                                                       // exit code success
 }
