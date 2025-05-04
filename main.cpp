4167bd330110cadbccedbb5be85a8b98ca94a023
benja2998 <benja2998@users.noreply.github.com>
2025-05-04 17:34:26 -0400

    Massive update to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-#include <iostream>                                                 // iostream contains std
+#include <iostream>
+#include <string>
+#include <cstdlib>
 
 static void banner()
 {
@@ -10,16 +12,44 @@ static void banner()
 |__|_|  /____/|____/__| |__||__|  \____/ \____/|____/    |__|        |__|    
       \/                                                                     
 
-)";                                                                 // multiline raw string
-    std::cout << banner << "\n";                                    // cout - character output
+)";
+    std::cout << banner << "\n";
+}
+
+static void menu()
+{
+    std::cout << "-- OPTIONS: --\n";
+    std::cout << "1. exit\n";
+    std::cout << "--          --\n";
+}
+
+static void read()
+{
+    std::string input;
+    std::cout << "enter an option: ";
+    std::getline(std::cin, input);
+    std::cout << "You chose: " << input << "\n";
+    std::cout << "Validating choice... ";
+    if (input == "1")
+    {
+        std::cout << "Valid choice!\n";
+        exit(0);
+    }
+    else 
+    {
+        std::cout << "Invalid choice.\n";
+        exit(1);
+    }
 }
 
 int main()
 {
-    std::cout << "Hello from Multitool++!\n";                       // cout - character output
-    std::cout << "C++ is a great language!\n";                      // cout - character output
+    std::cout << "Hello from Multitool++!\n";
+    std::cout << "C++ is a great language!\n";
 
-    banner();                                                       // calling banner function
-
-    return 0;                                                       // exit code success
+    banner();
+    menu();
+    read();
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 }
