2caa4f0b2eb508b6b4d6a756f958e955d4b336a0
benja2998 <benja2998@users.noreply.github.com>
2025-05-06 16:26:06 -0400

    More informative main.cpp

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ namespace console {
 #elif defined(__linux__) || defined(__APPLE__) || defined(__unix__)
         system("clear");
 #else
+        std::cout << "Unsupported OS. Please make sure you have modified the source code before compiling for another OS."
         exit(10);
 #endif
     }
